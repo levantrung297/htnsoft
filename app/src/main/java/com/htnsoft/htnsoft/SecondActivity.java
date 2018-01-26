250f991f3f30e755c2c5065d19fd3da3c3e74711
Trung Van <levantrung297@gmail.com>
2018-01-26 11:36:32 +0700

    Class Student

--- app/src/main/java/com/htnsoft/htnsoft/SecondActivity.java
+++ app/src/main/java/com/htnsoft/htnsoft/SecondActivity.java
@@ -6,22 +6,27 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class SecondActivity extends AppCompatActivity {
     Button btnlogin;
+    TextView txtKetQua;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_second);
         btnlogin = (Button) findViewById(R.id.btn_login);
-        btnlogin.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent;
-                intent = new Intent(SecondActivity.this, MainActivity.class);
-                startActivity(intent);
-            }
-        });
+        txtKetQua =(TextView) findViewById(R.id.txtketqua);
+
+        // Nhận kết quả từ intent
+
+        Intent intent;
+        String[] MangMonHoc;
+        intent = getIntent();
+        MangMonHoc = intent.getStringArrayExtra("Mangmonhoc");
+        String NoiDung = intent.getStringExtra("txtKetQua");
+        Student hs = (Student) intent.getSerializableExtra("hocsinh");
+        txtKetQua.setText(hs.getHoten()+" "+hs.getNgaysinh());
     }
 
     public SecondActivity() {
